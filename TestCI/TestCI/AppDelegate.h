01071a1be17333eb359206cbe86b4c2bd3e6bdeb
Timur Nurov <timurnurov@Timurs-MacBook-Pro.local>
2014-10-20 11:32:23 +0400

    start

---
+++ TestCI/TestCI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestCI
+//
+//  Created by Timur Nurov on 20/10/14.
+//  Copyright (c) 2014 Timur Nurov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
